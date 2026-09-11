28a873209b95a6777fcbfaf29db6a5753c3114a3
cheeflofer <ravneetgrewal1000@gmail.com>
2026-09-11 22:26:28 +0530

    ui done with docs

--- flask_api.py
+++ flask_api.py
@@ -1,12 +1,21 @@
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/docs")
+def docs():
+    return render_template("docs.html")
+
+@app.route("/assets/<path:filename>")
+def asset_file(filename):
+    return send_from_directory(os.path.join(app.root_path, "Assets"), filename)
 
 
 
